847aaedc130f9555f5e1fc4f5af2c4987185126c
baccrie <bakarerilwan1@gmail.com>
2023-08-20 12:26:58 +0100

    Task 1

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
-""" 1. Basic annotations - concat
-"""
+
+""""Basic Annotations - concat"""
 
 
 def concat(str1: str, str2: str) -> str:
-    """ Concats two strings together.
-    """
-    return str(str1) + str(str2)
+    """Returns a str by concating the args"""
+    return (str1 + str2)
