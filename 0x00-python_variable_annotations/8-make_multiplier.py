5a66aaabf5bf265950e6c72afc54dbc20daea138
baccrie <bakarerilwan1@gmail.com>
2023-08-20 14:16:48 +0100

    Task 8 corr

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -5,6 +5,6 @@
 from typing import Callable
 
 
-def make_multiplier(multiplier: float) -> Callable[float]:
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
     '''A funtion that returns a function that accepts float'''
     return lambda a: a * multiplier
