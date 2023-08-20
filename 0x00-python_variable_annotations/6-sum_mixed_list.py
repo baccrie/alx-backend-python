47cecae4b9840d718357c113505d500b5ab650ab
baccrie <bakarerilwan1@gmail.com>
2023-08-20 13:28:47 +0100

    Task 6 corr1

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -7,6 +7,6 @@ from functools import reduce
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     '''Returns a sum of floats and int args'''
     return reduce(lambda a, b: a + b, mxd_lst)
